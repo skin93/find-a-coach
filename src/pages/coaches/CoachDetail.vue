1f050cffd305041d6641afdf6326b586208dd84e
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-09-28 08:35:57 +0200

    Wire up pages

---
+++ src/pages/coaches/CoachDetail.vue
@@ -0,0 +1,5 @@
+<template>
+  DETAILS OF COACH
+  <router-view></router-view>
+  <router-link to="/coaches/c1/contact">Contact</router-link>
+</template>
---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    FILTER
+  </section>
+  <section>
+    <div class="controls">
+      <button>REFRESH</button>
+      <router-link to="/register">Register a Coach</router-link>
+    </div>
+    <ul>
+      LIST OF COACHES
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
