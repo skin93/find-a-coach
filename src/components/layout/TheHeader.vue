22d71c52327dac6c969516b02d4c3c6c1b462995
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-09-28 08:26:56 +0200

    Add TheHeader component

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,76 @@
+<template>
+  <header>
+    <nav>
+      <h1><router-link to="/">Find a Coach</router-link></h1>
+      <ul>
+        <li><router-link to="/coaches">All Coaches</router-link></li>
+        <li><router-link to="/requests">Requests</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+header {
+  width: 100%;
+  height: 5rem;
+  background-color: #3d008d;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  a {
+    text-decoration: none;
+    color: #f391e3;
+    display: inline-block;
+    padding: 0.75rem 1.5rem;
+    border: 1px solid transparent;
+
+    &:active,
+    &:hover,
+    &.router-link-active {
+      border: 1px solid #f391e3;
+    }
+  }
+
+  nav {
+    width: 90%;
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    h1 {
+      margin: 0;
+
+      a {
+        color: white;
+        margin: 0;
+
+        &:hover,
+        &:active,
+        &.router-link-active {
+          border-color: transparent;
+        }
+      }
+    }
+  }
+
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    li {
+      margin: 0 0.5rem;
+    }
+  }
+}
+</style>
