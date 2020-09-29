90dc7c07d175928e1fc557f83212b0813eda14b6
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-09-29 19:51:46 +0200

    Add Login & Signup pages

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -51,7 +51,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.form {
+form {
   margin: 1rem;
   border: 1px solid #ccc;
   border-radius: 12px;
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import CoachesList from "../pages/coaches/CoachesList.vue";
 import CoachRegistration from "../pages/coaches/CoachRegistration.vue";
 import ContactCoach from "../pages/requests/ContactCoach.vue";
 import RequestsReceived from "../pages/requests/RequestsReceived.vue";
+import UserAuth from "../pages/auth/UserAuth.vue";
 import NotFound from "../pages/NotFound.vue";
 
 const routes = [
@@ -23,6 +24,7 @@ const routes = [
   },
   {path: "/register", component: CoachRegistration},
   {path: "/requests", component: RequestsReceived},
+  {path: "/auth", component: UserAuth},
   {path: "/:notFound(.*)", component: NotFound},
 ];
 
