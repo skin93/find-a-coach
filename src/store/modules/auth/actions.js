6b9564204aa7d6ce1961f1bfaaf330592408ebde
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-09-29 20:27:11 +0200

    Add a login action

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,5 +1,32 @@
 export default {
-  login() {},
+  async login(context, payload) {
+    const res = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAfkFkR1fmaRdGce7moZOpPKxSbD7oAXUc",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    const data = await res.json();
+
+    if (!res.ok) {
+      const error = new Error(
+        data.message || "Failed to authenticate. Check your login data."
+      );
+      throw error;
+    }
+
+    context.commit("setUser", {
+      token: data.idToken,
+      userId: data.localId,
+      tokenExpiration: data.expiresIn,
+    });
+  },
   async signup(context, payload) {
     const res = await fetch(
       "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAfkFkR1fmaRdGce7moZOpPKxSbD7oAXUc",
