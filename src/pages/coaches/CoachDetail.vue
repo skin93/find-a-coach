bf365564547d828b1c7bab52c0087fe55ca9d90c
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-09-29 16:51:29 +0200

    Add route transition

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -1,33 +1,35 @@
 <template>
-  <section>
-    <base-card>
-      <h2>{{ fullName }}</h2>
-      <h3>${{ rate }}/hour</h3>
-    </base-card>
-  </section>
-  <section>
-    <base-card>
-      <header>
-        <h2>
-          Interested? Reach out now!
-        </h2>
-        <base-button link :to="contactLink">Contact</base-button>
-      </header>
-      <router-view></router-view>
-    </base-card>
-  </section>
-  <section>
-    <base-card>
-      <base-badge
-        v-for="area in selectedCoach.areas"
-        :key="area"
-        :type="area"
-        :title="area"
-      >
-      </base-badge>
-      <p>{{ description }}</p>
-    </base-card>
-  </section>
+  <div>
+    <section>
+      <base-card>
+        <h2>{{ fullName }}</h2>
+        <h3>${{ rate }}/hour</h3>
+      </base-card>
+    </section>
+    <section>
+      <base-card>
+        <header>
+          <h2>
+            Interested? Reach out now!
+          </h2>
+          <base-button link :to="contactLink">Contact</base-button>
+        </header>
+        <router-view></router-view>
+      </base-card>
+    </section>
+    <section>
+      <base-card>
+        <base-badge
+          v-for="area in selectedCoach.areas"
+          :key="area"
+          :type="area"
+          :title="area"
+        >
+        </base-badge>
+        <p>{{ description }}</p>
+      </base-card>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -57,7 +59,7 @@ export default {
   },
   created() {
     this.selectedCoach = this.$store.getters["coaches/coaches"].find(
-      (coach) => coach.id === this.id,
+      (coach) => coach.id === this.id
     );
   },
 };
