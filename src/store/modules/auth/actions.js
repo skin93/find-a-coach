061d02e0880da50797c28528da0a74989b539f85
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-09-29 22:02:41 +0200

    Add an autoLogin action

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,44 +1,32 @@
 export default {
   async login(context, payload) {
-    const res = await fetch(
-      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAfkFkR1fmaRdGce7moZOpPKxSbD7oAXUc",
-      {
-        method: "POST",
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
-
-    const data = await res.json();
-
-    if (!res.ok) {
-      const error = new Error(
-        data.message || "Failed to authenticate. Check your login data."
-      );
-      throw error;
-    }
-
-    context.commit("setUser", {
-      token: data.idToken,
-      userId: data.localId,
-      tokenExpiration: data.expiresIn,
+    return context.dispatch("auth", {
+      ...payload,
+      mode: "login",
     });
   },
   async signup(context, payload) {
-    const res = await fetch(
-      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAfkFkR1fmaRdGce7moZOpPKxSbD7oAXUc",
-      {
-        method: "POST",
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
+    return context.dispatch("auth", {
+      ...payload,
+      mode: "signup",
+    });
+  },
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAfkFkR1fmaRdGce7moZOpPKxSbD7oAXUc";
+    if (mode === "signup") {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAfkFkR1fmaRdGce7moZOpPKxSbD7oAXUc";
+    }
+    const res = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+    });
 
     const data = await res.json();
 
@@ -49,15 +37,30 @@ export default {
       throw error;
     }
 
+    localStorage.setItem("token", data.idToken);
+    localStorage.setItem("userId", data.localId);
+
     context.commit("setUser", {
       token: data.idToken,
       userId: data.localId,
       tokenExpiration: data.expiresIn,
     });
   },
+  autoLogin(context) {
+    const token = localStorage.getItem("token");
+    const userId = localStorage.getItem("userId");
+
+    if (token && userId) {
+      context.commit("setUser", {
+        token: token,
+        userId: userId,
+        tokenExpiration: null,
+      });
+    }
+  },
   logout(context) {
     context.commit("setUser", {
-      token: null,
+      toke: null,
       userId: null,
       tokenExpiration: null,
     });
