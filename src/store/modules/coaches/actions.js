c59cf799b1021548b4a01d55179da89e9bc7ee2c
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-09-29 13:54:19 +0200

    Send a PUT http request to store coach data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,7 +1,7 @@
 export default {
-  registerCoach(context, payload) {
+  async registerCoach(context, payload) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: payload.first,
       lastName: payload.last,
       description: payload.desc,
@@ -9,6 +9,23 @@ export default {
       areas: payload.areas,
     };
 
-    context.commit("registerCoach", coachData);
+    const res = await fetch(
+      `https://find-a-coach-a9143.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: "PUT",
+        body: JSON.stringify(coachData),
+      },
+    );
+
+    //  const data = await res.json();
+
+    if (!res.ok) {
+      // error
+    }
+
+    context.commit("registerCoach", {
+      ...coachData,
+      id: userId,
+    });
   },
 };
